9b5f82aa13fe7a4694d088e09fac5f8206fb4c6d
katina-anachkova <katianachkova@gmail.com>
2021-12-05 18:12:46 +0200

    change welcome msg

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome KATINGA</h1>
+        <h1 className={styles.title}>Welcome!</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
