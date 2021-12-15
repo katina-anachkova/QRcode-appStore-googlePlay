b6c2758be49e4967875ba7e4d3b4fce29613aa53
katina-anachkova <katianachkova@gmail.com>
2021-12-15 11:44:46 +0200

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9f9b4941820e261f"; }
+/******/ 		__webpack_require__.h = function() { return "b962792ed9c42a0d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
